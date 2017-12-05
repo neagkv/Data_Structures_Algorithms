89e825869d9b35a171efa8017ebb7d29ce999b27
Kevin Neag <neagkv@gmail.com>
2017-12-04 22:31:39 -0500

    new binary tree

---
+++ src/data_structures/binarytree/BinaryTree.java
@@ -0,0 +1,12 @@
+package data_structures.binarytree;
+
+/**
+ * @author Kevin Neag
+ */
+public class BinaryTree {
+
+    int key;
+    Node left;
+    Node right;
+
+}
---
+++ src/data_structures/binarytree/Node.java
@@ -0,0 +1,7 @@
+package data_structures.binarytree;
+
+/**
+ * @author Kevin Neag
+ */
+public class Node {
+}
