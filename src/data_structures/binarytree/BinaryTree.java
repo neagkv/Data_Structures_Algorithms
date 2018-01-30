fa3dab77a56af4039d501360f36eabeab31f3948
Kevin Neag <neagkv@gmail.com>
2018-01-30 15:53:44 -0500

    basic binary tree

--- src/data_structures/binarytree/BinaryTree.java
+++ src/data_structures/binarytree/BinaryTree.java
@@ -5,8 +5,37 @@ package data_structures.binarytree;
  */
 public class BinaryTree {
 
-    int key;
-    Node left;
-    Node right;
+    Node root;
 
+    BinaryTree(int key){
+
+        root = new Node(key);
+    }
+
+    BinaryTree(){
+
+        root = null;
+    }
+
+
+    public static void main(String[] args) {
+
+        BinaryTree binaryTree = new BinaryTree();
+
+        binaryTree.root = new Node(1);
+
+        binaryTree.root.left = new Node(2);
+
+        binaryTree.root.left = new Node(3);
+
+        binaryTree.root.left = new Node(4);
+
+        binaryTree.root.right = new Node (11);
+
+        binaryTree.root.right.left = new Node(12);
+
+        binaryTree.root.right.right = new Node(13);
+
+
+    }
 }
--- src/data_structures/binarytree/Node.java
+++ src/data_structures/binarytree/Node.java
@@ -4,4 +4,14 @@ package data_structures.binarytree;
  * @author Kevin Neag
  */
 public class Node {
+
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int item){
+
+        key = item;
+        left = right = null;
+    }
 }
